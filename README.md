85dce88d358103b3123974cb9de27464abc5a437
Maximilian Temeschinko <maxiteme@peoplesmarkets.com>
2024-09-27 13:26:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# bye - test service
+
+## init
+
+`go mod init bye && go mod tidy`
+
+## build
+
+`go build main.go`
+
+## prepare deployment
+
+`./get_envs.sh`
+
+## deploy
+
+### dev
+
+`act -P ubuntu-latest=local/act_runner --pull=false --secret-file=.env.dev`
+
+### stg
+
+`act -P ubuntu-latest=local/act_runner --pull=false --secret-file=.env.stg`
+
+### prod
+
+`act -P ubuntu-latest=local/act_runner --pull=false --secret-file=.env.prod`
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type ByeHandler struct {
+	env string
+}
+
+func (hh ByeHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("Got request %v\n", req)
+	fmt.Fprintf(w, "bye from environment: \"%v\"\n", hh.env)
+}
+
+func health(w http.ResponseWriter, _ *http.Request) {
+	io.WriteString(w, "OK")
+}
+
+func main() {
+	var ENV = os.Getenv("ENV")
+	if len(ENV) < 1 {
+		log.Fatal("ERROR: please provide environment varialbe 'ENV'")
+	}
+
+	hh := ByeHandler{env: ENV}
+
+	http.Handle("/bye", hh)
+	http.HandleFunc("/health", health)
+
+	const addr = ":8000"
+
+	fmt.Printf("HTTP server listening on %v\n", addr)
+	http.ListenAndServe(addr, nil)
+}
